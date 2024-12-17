405dc7fb98b270211de03497f0a646ffc021d5e7
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2024-12-17 21:01:00 +0700

    feat(component): add thumbnail component

---
+++ components/feature/VThumbnail/VThumbnail.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="bg-purple-500 hover:bg-purple-600 transition-colors rounded-full size-10 font-bold text-slate-50 grid place-items-center cursor-pointer"
+  >
+    D
+  </div>
+</template>
