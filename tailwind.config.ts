4620bea402236dea5a1ac5b8d73ea286eeff491f
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2024-12-17 22:13:53 +0700

    feat(style): add background image for board container

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        "board-background": "url('/assets/images/background.jpg')",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
