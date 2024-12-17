1ef15649042c832305d67d1c7394c393e90fc316
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2024-12-17 21:01:35 +0700

    feat(component): add container component

---
+++ components/feature/VContainer/VContainer.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import type { Props } from ".";
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <section :class="cn('max-w-screen-xl mx-auto', props.class)">
+    <slot />
+  </section>
+</template>
