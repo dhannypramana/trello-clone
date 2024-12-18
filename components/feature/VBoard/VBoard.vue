3dd367a16d3f4708f25f65679e889ba0c3cc0ef5
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2024-12-18 16:27:16 +0700

    feat(board): define props for accepting title props in vboard component

--- components/feature/VBoard/VBoard.vue
+++ components/feature/VBoard/VBoard.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
 import { Ellipsis as VEllipsis } from "lucide-vue-next";
+import type { Props } from ".";
+
+defineProps<Props>();
 </script>
 
 <template>
   <div
-    class="bg-gray-300 rounded-md w-full md:w-1/5 p-2.5 space-y-2 flex-shrink-0 h-fit fixed cursor-move select-none"
+    class="bg-gray-300 rounded-md w-full md:w-1/3 lg:w-1/5 p-2.5 space-y-2 flex-shrink-0 h-fit cursor-move select-none"
   >
-    <!-- Header -->
     <div class="flex items-center justify-between px-4 py-1">
-      <h3 class="font-semibold">On Progress</h3>
+      <h3 class="font-semibold">{{ title }}</h3>
       <VEllipsis class="cursor-pointer" />
     </div>
 
-    <!-- Content -->
     <VBoardItem />
 
-    <!-- Footer -->
     <VButton variant="ghost" class="w-full justify-start">Add a card</VButton>
   </div>
 </template>
