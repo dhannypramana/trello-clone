66ec7513f9e3ed25abd0c1ac2ee31f1e79e46861
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2024-12-18 14:41:07 +0700

    feat(board): create container for board

--- app.vue
+++ app.vue
@@ -2,8 +2,12 @@
   <section class="max-h-screen overflow-hidden">
     <VNavbar />
 
-    <main class="min-h-screen bg-board-background bg-cover bg-center">
+    <main class="min-h-screen bg-board-background bg-cover bg-center space-y-6">
       <BoardMeta />
+
+      <VContainer class="min-h-screen flex overflow-x-scroll space-x-4">
+        <VBoard />
+      </VContainer>
     </main>
   </section>
 </template>
