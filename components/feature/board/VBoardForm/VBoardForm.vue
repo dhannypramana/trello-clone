372f9c903d6fcf96c5d0ad6db23561c79dc31ee4
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2024-12-19 16:04:55 +0700

    feat(create): create post board form creation to connect repo and use case, fetch board data when creating is in success

--- components/feature/board/VBoardForm/VBoardForm.vue
+++ components/feature/board/VBoardForm/VBoardForm.vue
@@ -2,22 +2,37 @@
 import { X } from "lucide-vue-next";
 import type { Emit } from ".";
 
+const { title } = useBoardField();
+const { postBoard } = useBoardCreate();
+
 const VBoardFormTrigger = resolveComponent("VBoardFormTrigger");
 const emit = defineEmits<Emit>();
 
 function toogleComponent() {
   emit("toogle", VBoardFormTrigger);
 }
+
+async function submitBoardForm() {
+  await postBoard(title.value);
+  emit("toogle", VBoardFormTrigger);
+}
 </script>
 
 <template>
   <VCard>
-    <VInput placeholder="Enter list title ..." :auto-focus="true" />
-    <div class="flex items-center space-x-2">
-      <VButton>Add list</VButton>
-      <VButton variant="unstyled" @click="toogleComponent">
-        <X />
-      </VButton>
-    </div>
+    <form @submit.prevent="submitBoardForm" class="space-y-2">
+      <VInput
+        v-model="title"
+        placeholder="Enter list title ..."
+        :auto-focus="true"
+      />
+
+      <div class="flex items-center space-x-2">
+        <VButton type="submit">Add list</VButton>
+        <VButton type="button" variant="unstyled" @click="toogleComponent">
+          <X />
+        </VButton>
+      </div>
+    </form>
   </VCard>
 </template>
