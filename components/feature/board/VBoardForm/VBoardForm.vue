6e6ff8572f7c26f268117400a85cb9d293af9271
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2024-12-19 14:33:28 +0700

    feat(board): create board form for insertion

---
+++ components/feature/board/VBoardForm/VBoardForm.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { X } from "lucide-vue-next";
+import type { Emit } from ".";
+
+const VBoardFormTrigger = resolveComponent("VBoardFormTrigger");
+const emit = defineEmits<Emit>();
+
+function toogleComponent() {
+  emit("toogle", VBoardFormTrigger);
+}
+</script>
+
+<template>
+  <VCard>
+    <VInput placeholder="Enter list title ..." />
+    <div class="flex items-center space-x-2">
+      <VButton>Add list</VButton>
+      <VButton variant="unstyled" @click="toogleComponent">
+        <X />
+      </VButton>
+    </div>
+  </VCard>
+</template>
