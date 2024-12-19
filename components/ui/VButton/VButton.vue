9b079857219162339a9dbd3115c032406b1898ec
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2024-12-19 14:29:23 +0700

    feat(ui): add button component

---
+++ components/ui/VButton/VButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Primitive } from "radix-vue";
+import { buttonVariants, type Props } from ".";
+
+const props = withDefaults(defineProps<Props>(), {
+  as: "button",
+});
+</script>
+
+<template>
+  <Primitive
+    :as="as"
+    :as-child="asChild"
+    :class="cn(buttonVariants({ variant, size }), props.class)"
+  >
+    <slot />
+  </Primitive>
+</template>
