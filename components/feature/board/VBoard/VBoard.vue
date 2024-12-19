9747d200ecb23d7b9e3144fb58bc2010246e554d
Dhanny Adhi Pramana <p.dhannypramana@gmail.com>
2024-12-19 14:32:25 +0700

    refactor(component): refactoring component dir

--- components/feature/board/VBoard/VBoard.vue
+++ components/feature/board/VBoard/VBoard.vue
@@ -6,9 +6,7 @@ defineProps<Props>();
 </script>
 
 <template>
-  <div
-    class="bg-gray-300 rounded-md w-full md:w-1/3 lg:w-1/5 p-2.5 space-y-2 flex-shrink-0 h-fit cursor-move select-none"
-  >
+  <VCard>
     <div class="flex items-center justify-between px-4 py-1">
       <h3 class="font-semibold">{{ title }}</h3>
       <VEllipsis class="cursor-pointer" />
@@ -17,5 +15,5 @@ defineProps<Props>();
     <VBoardItem />
 
     <VButton variant="ghost" class="w-full justify-start">Add a card</VButton>
-  </div>
+  </VCard>
 </template>
---
+++ components/feature/shared/VCard/VCard.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import type { Props } from ".";
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div
+    :class="
+      cn(
+        'bg-gray-300 rounded-md w-full md:w-1/3 lg:w-1/5 p-2.5 space-y-2 flex-shrink-0 h-full max-h-screen overflow-y-scroll select-none',
+        props.class
+      )
+    "
+  >
+    <slot />
+  </div>
+</template>
